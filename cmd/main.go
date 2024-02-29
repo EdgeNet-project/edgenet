f6aa81b805e1fe36b6ec95c10404c8e4318c7fc8
ubombar <ufukbombar@gmail.com>
2024-02-29 11:45:10 +0100

    Add tenant resource quota controller and crd

--- cmd/main.go
+++ cmd/main.go
@@ -96,6 +96,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Tenant")
 		os.Exit(1)
 	}
+	if err = (&controller.TenantResourceQuotaReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "TenantResourceQuota")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
