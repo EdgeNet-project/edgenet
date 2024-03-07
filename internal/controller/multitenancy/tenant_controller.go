52fe4b30960bd34af1fd2b937c4c64f5909a8cc5
ubombar <ufukbombar@gmail.com>
2024-03-07 09:11:18 +0100

    Add tenant controller

--- internal/controller/multitenancy/tenant_controller.go
+++ internal/controller/multitenancy/tenant_controller.go
@@ -22,9 +22,10 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	multitenancyv1 "github.com/edgenet-project/edgenet-software/api/multitenancy/v1"
+	"github.com/edgenet-project/edgenet-software/internal/multitenancy/v1"
+	"github.com/edgenet-project/edgenet-software/internal/utils"
 )
 
 // TenantReconciler reconciles a Tenant object
@@ -33,7 +34,12 @@ type TenantReconciler struct {
 	Scheme *runtime.Scheme
 }
 
+// These are required to have the permissions.
 //+kubebuilder:rbac:groups=multitenancy.edge-net.io,resources=tenants,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="rbac.authorization.k8s.io",resources=rolebindings,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="networking.k8s.io",resources=networkpolicies;clusternetworkpolicies,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="crd.antrea.io",resources=clusternetworkpolicies,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="",resources=namespaces,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=multitenancy.edge-net.io,resources=tenants/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=multitenancy.edge-net.io,resources=tenants/finalizers,verbs=update
 
@@ -45,11 +51,46 @@ type TenantReconciler struct {
 // the user.
 //
 // For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.17.0/pkg/reconcile
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.16.3/pkg/reconcile
 func (r *TenantReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	tenant := multitenancyv1.Tenant{}
+	isMarkedForDeletion, reconcileResult, err := utils.GetResourceWithFinalizer(ctx, r.Client, &tenant, req.NamespacedName)
 
-	// TODO(user): your logic here
+	if !utils.IsObjectInitialized(&tenant) {
+		return reconcileResult, err
+	}
+
+	multiTenancyManager, err := multitenancy.NewMultiTenancyManager(ctx, r.Client)
+
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	if isMarkedForDeletion {
+		// Do a cleanup and allow tenant object for deletion
+		if err := multiTenancyManager.TenantCleanup(ctx, &tenant); err != nil {
+			return ctrl.Result{Requeue: true}, err
+		}
+
+		return utils.AllowObjectDeletion(ctx, r.Client, &tenant)
+	} else {
+		// Create a core namespace for the tenant.
+		if err := multiTenancyManager.CreateCoreNamespaceLocal(ctx, &tenant); err != nil {
+			return ctrl.Result{Requeue: true}, err
+		}
+
+		// Create the role binding in the core namespace of the tenant.
+		if err := multiTenancyManager.CreateTenantAdminRoleBinding(ctx, &tenant); err != nil {
+			return ctrl.Result{Requeue: true}, err
+		}
+
+		// Create the network policy. This restricts pod communication. Don't need to clean after
+		// deletion of the tenant.
+		if err := multiTenancyManager.CreateTenantNetworkPolicy(ctx, &tenant); err != nil {
+			return ctrl.Result{Requeue: true}, err
+		}
+
+	}
 
 	return ctrl.Result{}, nil
 }
